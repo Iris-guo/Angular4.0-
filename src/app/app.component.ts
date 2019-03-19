19c4c5b5e215b0eb3a5610fca99603edc4e41e16
Iris-guo <188105993632163.com>
2019-03-19 16:13:38 +0800

    Angular 4.0 从入门到实战 打造股票管理网站

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import construct = Reflect.construct;
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'router';
+
+  constructor(private router: Router) {
+  }
+  toProductDetails() {
+    this.router.navigate(['/product', 2]);
+  }
+}
